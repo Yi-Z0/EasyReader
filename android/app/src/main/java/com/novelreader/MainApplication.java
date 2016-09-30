b9b8d165526d00ab831f23b9e69d58a5e479832b
Yi Zhang <zy438200@qq.com>
2016-09-30 17:15:49 +0800

    no message

--- android/app/src/main/java/com/novelreader/MainApplication.java
+++ android/app/src/main/java/com/novelreader/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.react.rnspinkit.RNSpinkitPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
@@ -25,7 +26,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new RNSpinkitPackage(),
-           new MainReactPackage()
+           new MainReactPackage(),
+            new VectorIconsPackage()
       );
     }
   };
