6690778bd4d48fab3ccbd5b44a98074d1cb093d8
Yi Zhang <zy438200@qq.com>
2016-10-06 11:53:11 +0800

    no message

--- src/parser/parseArticleContent.js
+++ src/parser/parseArticleContent.js
@@ -2,15 +2,23 @@ import crawler from './crawler';
 import {getRuleByUrl} from '../rules';
 
 export default async function parseArticleContent(url:string,retry:number = 3):Promise<string> {
- 
- let rule = getRuleByUrl(url);
- let $:cheerio = await crawler(url);
- 
- let content = rule.articleContentRule($);
-
- if (content.length<5 && retry>0) {
-   return parseArticleContent(url,retry-1);
- }
- 
- return content;
+ //get data from db
+ //save data to db after 
+   let rule = getRuleByUrl(url);
+   let content = '';
+   try{
+     let $:cheerio = await crawler(url);
+     
+     content = rule.articleContentRule($);
+   }catch(e){
+     if (retry == 0) {
+       return e;
+     }
+   }
+   
+   if (content.length<5 && retry>0) {
+     return parseArticleContent(url,retry-1);
+   }
+   
+   return content;
 }
