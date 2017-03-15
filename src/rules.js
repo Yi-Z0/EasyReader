608c5b7a19812ced5fcb687749d12b6f65a6997e
Yi Zhang <zy438200@qq.com>
2017-03-15 19:33:31 +0800

    增加一个小说网站的支持

--- src/rules.js
+++ src/rules.js
@@ -102,6 +102,25 @@ const rules = [
       return $("#content").text().replace('readx();&nbsp;&nbsp;&nbsp;&nbsp;','');
     },
   },
+  {
+    domain: `www.geilwx.com`,
+    encode:'gbk',
+    //工作在搜索引擎提取的url中
+    directoryUrlRegexp: /^\/GeiLi\/\d+\/\d+\/$/,
+    //工作在搜索引擎提取页面
+    titleRule: /.*(?=最新章节)/,
+    //工作在列表页面
+    authorRule: $=>$("body > div.TabCss > dl > p").text().replace(/写的小说.*$/,'').replace('作者：',''),
+    //工作在列表页面,返回一个a标签
+    articleLinkRule: $=>$("body > div.TabCss > dl > dd > a"),
+    //工作在文章页面
+    articleContentRule: $=>{
+      $('script,div,h1').replaceWith('');
+      $("br").replaceWith("\r\n");
+
+      return $("body").text().replace('readx();&nbsp;&nbsp;&nbsp;&nbsp;','').trim();
+    },
+  },
 ];
 export default rules;
 
